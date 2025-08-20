9fc3b7339962e35c9f25d6837179eb79329d9b87
Maya OH <mayaoh2000@gmail.com>
2025-08-20 13:11:14 -0400

    Python  solution to problem 14

--- leetcode-solutions/Python/Array/14_LongestCommonPrefix.py
+++ leetcode-solutions/Python/Array/14_LongestCommonPrefix.py
@@ -3,8 +3,20 @@ from typing import List
 
 
 def longestCommonPrefix(strs: List[str]) -> str:
-    
+    #Storing first word
+    prefix = strs[0]
 
+    # Compare prefix against each remaining word
+    for word in strs[1:]:
+        # Shorten prefix from the end until current word starts with it
+        while not word.startswith(prefix):
+            #slicing [start:end] (-1 start from end of word)
+            prefix = prefix[:-1] #removed last char
+        
+            #Return empty string for no common prefix
+            if prefix == "":
+                return ""      
+    return prefix
 
 
 # Example test cases
