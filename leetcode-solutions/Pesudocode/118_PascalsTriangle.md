f7f6db4cd0de3d2a4b0dc3228f317c61f4137e94
Maya OH <mayaoh2000@gmail.com>
2025-08-28 20:59:26 -0400

    Added templates for problem 118

---
+++ leetcode-solutions/Pesudocode/118_PascalsTriangle.md
@@ -0,0 +1,12 @@
+# Problem 118: Pascal's Triangle
+Difficulty: Easy
+Category: Array
+Approach: 
+
+# Goal
+
+
+# Example
+
+
+# Approach
---
+++ leetcode-solutions/Python/Array/118_PascalsTriangle.py
@@ -0,0 +1,17 @@
+# ID: 118
+# Problem: Pascal's Triangle
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/pascals-triangle/
+# Approach: 
+
+from typing import List, Optional
+
+
+def generate(self, numRows: int) -> List[List[int]]:
+
+
+
+
+if __name__ == "__main__":
+    print(generate(5))  # Output [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+    # print(generate(1))  # Output [[1]]
