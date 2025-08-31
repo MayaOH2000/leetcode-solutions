9b98d897bea3f91ca187d6fb22e00754deb06468
Maya OH <mayaoh2000@gmail.com>
2025-08-31 10:42:37 -0400

    Added templates for problem 121

---
+++ leetcode-solutions/Python/Array/121_BestTimetoBuyandSellStock.py
@@ -0,0 +1,14 @@
+# ID: 121
+# Problem: Best Time to Buy and Sell Stock
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Approch: 
+
+from typing import List
+
+def maxProfit(prices: List[int]) -> int:
+
+
+if __name__ == "__main__":
+    print(maxProfit([7,1,5,3,6,4])) # Output 5
+    # print(maxProfit([7,6,4,3,1]))   # Output 0
