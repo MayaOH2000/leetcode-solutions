8abdbe8692309d8a002c086fe5e501d5a36beda3
Maya OH <mayaoh2000@gmail.com>
2025-08-31 10:30:16 -0400

    added python solution to problem 119

--- leetcode-solutions/Python/Array/119_PascalsTriangleII.py
+++ leetcode-solutions/Python/Array/119_PascalsTriangleII.py
@@ -5,9 +5,28 @@
 
 from typing import List
 
+# Similar approach as problem 118 except just need the row
+
 def getRow(rowIndex: int) -> List[int]:
+    # Row 0 is always just [1]
+    row = [1]   
 
+    # Make rows (add + 1 to include last row value)
+    for rindex in range(1,rowIndex + 1):
+        new_row = [1]
 
+        # Middle elements in new row
+        for num in range (1, rindex ):
+            new_row.append(row[num -1] + row[num])
+        
+        # Adding 1 to end of row
+        new_row.append(1)
+
+        # Update row to the new row
+        row = new_row
+
+    # Return the final row (at rowIndex)
+    return row
 
 if __name__ == "__main__":
     print(getRow(3))  # Output [1,3,3,1]
