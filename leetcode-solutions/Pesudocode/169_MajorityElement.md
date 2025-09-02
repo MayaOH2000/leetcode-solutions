40c036d0db5e5e94574f7d272338bcfa8d0ecdcb
Maya OH <mayaoh2000@gmail.com>
2025-09-02 15:37:20 -0400

    Added templates for problem 169

---
+++ leetcode-solutions/Pesudocode/169_MajorityElement.md
@@ -0,0 +1,20 @@
+# Problem 169: Majority Element
+Difficulty: Easy
+Category: Array 
+Approach: 
+
+# Goal
+
+
+# Example
+Example 1:
+
+    Input: nums = [3,2,3]
+    Output: 3
+
+Example 2:
+
+    Input: nums = [2,2,1,1,1,2,2]
+    Output: 2
+
+# Approach
---
+++ leetcode-solutions/Python/Array/169_MajorityElement.py
@@ -0,0 +1,14 @@
+# ID: 169
+# Problem: Majority Element
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/majority-element/
+# Approach:
+
+from typing import List
+
+def majorityElement(nums: List[int]) -> int:
+
+
+
+if __name__ == "__main__":
+    print(majorityElement[3,2,3])   # Output 3
