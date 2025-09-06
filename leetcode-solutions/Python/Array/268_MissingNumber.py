c3662624fc1b64c292c11da253ee5bd69199f91a
Maya OH <mayaoh2000@gmail.com>
2025-09-06 12:54:40 -0400

    Added templetes for problem 268

---
+++ leetcode-solutions/Python/Array/268_MissingNumber.py
@@ -0,0 +1,14 @@
+# ID: 268
+# Problem: Missing Number
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/missing-number/
+# Approach: 
+
+from typing import List
+
+def missingNumber(nums: List[int]) -> int:
+
+
+
+if __name__ == "__main__":
+    print()
