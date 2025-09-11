ecc93f38483ab6698abe513a48aed1b7ef8ee283
Maya OH <mayaoh2000@gmail.com>
2025-09-11 15:30:45 -0400

    templates for problem 349

---
+++ 349_IntersectionOfTwoArrays.py
@@ -0,0 +1,14 @@
+# ID: 349
+# Problem: Intersection of Two Arrays
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/intersection-of-two-arrays/
+# Approach:
+
+from typing import List
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+    
+
+
+if __name__ == "__main__":
+    print()
