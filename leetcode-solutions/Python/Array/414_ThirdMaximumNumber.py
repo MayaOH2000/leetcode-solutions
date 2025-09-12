329f5e2ba411abb450e2c2f22d1a86c0e57d7e6d
Maya OH <mayaoh2000@gmail.com>
2025-09-12 13:35:02 -0400

    Templates for problem 414

---
+++ leetcode-solutions/Python/Array/414_ThirdMaximumNumber.py
@@ -0,0 +1,13 @@
+# ID: 414
+# Problem: Third Maximum Number
+# Difficulty: Easy
+# Link: https://leetcode.com/problems/third-maximum-number/
+# Approach:
+
+from typing import List
+
+def thirdMax(nums: List[int]) -> int:
+
+
+if __name__ == "__main__":
+    print(thirdMax([3,2,1])) # Output 1
